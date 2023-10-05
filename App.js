3975b0b246959b535a925a30240adc1cedfdfd6d
TrongTre07 <trongtruth2003@gmail.com>
2023-10-06 00:16:01 +0700

    completed screen detail and change profile

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Detail from './src/component/main/product/Detail';
+import EditProfile from './src/component/main/profile/EditProfile';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <EditProfile />
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ src/component/main/profile/EditProfile.js
@@ -0,0 +1,75 @@
+import {StyleSheet, Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {editProfileStyle} from './styles/editProfileStyle';
+import Icon from 'react-native-vector-icons/FontAwesome6';
+
+const EditProfile = () => {
+  return (
+    <View style={editProfileStyle.container}>
+      {/* Image avatar */}
+      <Image
+        source={require('../../../assets/images/americano.png')}
+        style={editProfileStyle.imageProfile}
+      />
+
+      {/* input name view */}
+      <View style={editProfileStyle.textInputContainer}>
+        <Text style={editProfileStyle.textLableInput}>Tên</Text>
+        <View style={editProfileStyle.inputContainer}>
+          <TextInput
+            style={editProfileStyle.input}
+            placeholder="Tên của bạn "
+            placeholderTextColor="#999"
+          />
+          <Icon
+            name="user"
+            size={20}
+            color="#999"
+            style={editProfileStyle.icon}
+          />
+        </View>
+      </View>
+
+      {/* input Email view */}
+      <View style={editProfileStyle.textInputContainer}>
+        <Text style={editProfileStyle.textLableInput}>E-mail</Text>
+        <View style={editProfileStyle.inputContainer}>
+          <TextInput
+            style={editProfileStyle.input}
+            placeholder="Email của bạn "
+            placeholderTextColor="#999"
+          />
+          <Icon
+            name="envelope"
+            size={20}
+            color="#999"
+            style={editProfileStyle.icon}
+          />
+        </View>
+      </View>
+
+      {/* input Email view */}
+      <View style={editProfileStyle.textInputContainer}>
+        <Text style={editProfileStyle.textLableInput}>Số điện thoại</Text>
+        <View style={editProfileStyle.inputContainer}>
+          <TextInput
+            style={editProfileStyle.input}
+            placeholder="SDT của bạn "
+            placeholderTextColor="#999"
+          />
+          <Icon
+            name="envelope"
+            size={20}
+            color="#999"
+            style={editProfileStyle.icon}
+          />
+        </View>
+      </View>
+      <TouchableOpacity style={editProfileStyle.buttonSaveProfile}>
+        <Text style={editProfileStyle.textSaveProfile}>Đặt Lại Hồ Sơ</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default EditProfile;
