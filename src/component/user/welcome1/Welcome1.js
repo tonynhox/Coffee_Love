27c3d7dedb81625ae502d9edccee1ec858ac01d9
tonynhox <anhhuytran1470@gmail.com>
2023-10-06 23:38:35 +0700

    fix eror

--- src/component/user/welcome1/Welcome1.js
+++ src/component/user/welcome1/Welcome1.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image} from 'react-native'
+import { StyleSheet, Text, View, Image,Pressable} from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { styles } from './styles'
