97d469be1c20ecd0dbc82979d0f01f8514352f1c
tonynhox <anhhuytran1470@gmail.com>
2023-12-18 21:43:01 +0700

    fix voucher

--- src/components/main/voucher/ListVoucher.js
+++ src/components/main/voucher/ListVoucher.js
@@ -8,8 +8,9 @@ import {
   TouchableOpacity,
   ImageBackground,
   Alert,
+  RefreshControl,
 } from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {styles} from './styles';
 import {useDispatch, useSelector} from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -22,6 +23,7 @@ import moment from 'moment';
 import Icon6 from 'react-native-vector-icons/FontAwesome6';
 import {getChangeScoreFetch} from '../../../redux/reducers/slices/scoreSlide';
 import {getVoucherFetch} from '../../../redux/reducers/slices/voucherSlide';
+import {getOneUserFetch} from '../../../redux/reducers/slices/userSlice';
 const colorCartTV = number => {
   switch (true) {
     case number < 200:
@@ -123,6 +125,18 @@ const ListVoucher = () => {
     );
   };
 
+  const [refreshing, setRefreshing] = useState(false);
+
+  const fetchDiem = () => {
+    setRefreshing(true);
+    Promise.all([
+      dispatch(getVoucherFetch({id_user: user.id_user})),
+      dispatch(getOneUserFetch({id_user: user.id_user})),
+    ]).then(() => {
+      setRefreshing(false);
+    });
+  };
+
   const handleChangeScore = item => {
     const {diem, _id, ten_voucher, gia_tri, ngay_ket_thuc} = item;
     dispatch(
@@ -171,128 +185,137 @@ const ListVoucher = () => {
   return !user ? (
     <ListVoucherNotLG />
   ) : (
-    <ScrollView style={styles.container}>
-      <LinearGradient
-        style={[styles.fistCard]}
-        start={{x: 0, y: 0}}
-        end={{x: 0.7, y: 1}}
-        colors={colorCartTV(user?.diem_thanh_vien).colorCard}>
-        {/* <View style={styles.fistCard}> */}
+    dataScore && allVoucher && (
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={() => fetchDiem()}
+          />
+        }
+        style={styles.container}>
+        <LinearGradient
+          style={[styles.fistCard]}
+          start={{x: 0, y: 0}}
+          end={{x: 0.7, y: 1}}
+          colors={colorCartTV(user?.diem_thanh_vien).colorCard}>
+          {/* <View style={styles.fistCard}> */}
 
-        <Text style={[styles.txtfc, {fontSize: 20, fontWeight: '600'}]}>
-          Ưu đãi
-        </Text>
-        <Text
-          style={[
-            styles.txtfc,
-            {marginVertical: 10, fontSize: 27, fontWeight: '700'},
-          ]}>
-          {user?.hang_thanh_vien}
-        </Text>
-        <View style={styles.txtfc2}>
-          <Text style={[styles.txtfc, {fontSize: 15}]}>
-            {user?.tich_diem} Điểm
+          <Text style={[styles.txtfc, {fontSize: 20, fontWeight: '600'}]}>
+            Ưu đãi
           </Text>
-          <TouchableOpacity
-            onPress={() => navigation.navigate('AllVoucher')}
-            style={styles.btnvc}>
-            <Icon
-              name="ticket-percent-outline"
-              style={[{color: '#e77300', fontSize: 20}]}
-            />
-            <Text style={styles.txtfc3}>Voucher của tôi</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.cardRowfc}>
-          <BarcodeGenerator height={60} ma_khach_hang={user?.ma_khach_hang} />
-          {/* <Icon name="barcode" style={styles.barcode} />
+          <Text
+            style={[
+              styles.txtfc,
+              {marginVertical: 10, fontSize: 27, fontWeight: '700'},
+            ]}>
+            {user?.hang_thanh_vien}
+          </Text>
+          <View style={styles.txtfc2}>
+            <Text style={[styles.txtfc, {fontSize: 15}]}>
+              {user?.tich_diem} Điểm
+            </Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('AllVoucher')}
+              style={styles.btnvc}>
+              <Icon
+                name="ticket-percent-outline"
+                style={[{color: '#e77300', fontSize: 20}]}
+              />
+              <Text style={styles.txtfc3}>Voucher của tôi</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.cardRowfc}>
+            <BarcodeGenerator height={60} ma_khach_hang={user?.ma_khach_hang} />
+            {/* <Icon name="barcode" style={styles.barcode} />
           <Text style={styles.txtfc4}>1234565</Text> */}
+          </View>
+          <Text style={[styles.txtfc, {fontSize: 12, marginTop: 10}]}>
+            Còn {scoreUp(user.diem_thanh_vien)} điểm nữa bạn sẻ thăng hạng
+          </Text>
+          <Text style={[styles.txtfc, {fontSize: 12}]}>
+            Đổi quà không ảnh hưởng tới việc thăng hạng của bạn
+          </Text>
+          <Text style={[styles.txtfc, {fontSize: 12}]}>
+            Hãy dùng điểm để đổi ưu đãi nhé
+          </Text>
+          {/* </View> */}
+        </LinearGradient>
+
+        <View style={styles.card}>
+          <View style={styles.cardRow}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('WheelOfFortune')}
+              style={styles.cardExtention}>
+              <Icon
+                name="crown-outline"
+                style={[styles.icon, {color: 'orange', fontSize: 26}]}
+              />
+              <Text style={styles.txtExtention}>Vòng quay </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.cardExtention}
+              onPress={() => navigation.navigate('AllScore')}>
+              <Icon
+                name="gift-outline"
+                style={[styles.icon, {color: '#FF4500', fontSize: 26}]}
+              />
+              <Text style={styles.txtExtention}>Đổi điểm</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.cardRow}>
+            <TouchableOpacity
+              style={styles.cardExtention}
+              onPress={() => {
+                dispatch(getHistoryScoreFetch({id_user: user.id_user}));
+                navigation.navigate('ScoreHistory');
+              }}>
+              <Icon
+                name="file-search-outline"
+                style={[styles.icon, {color: '#FF8C00', fontSize: 26}]}
+              />
+              <Text style={styles.txtExtention}>Lịch sử điểm</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.cardExtention}>
+              <Icon
+                name="shield-account-outline"
+                style={[styles.icon, {color: '#1E90FF', fontSize: 26}]}
+              />
+              <Text style={styles.txtExtention}>Quyền Lợi của bạn</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <Text style={[styles.txtfc, {fontSize: 12, marginTop: 10}]}>
-        Còn {scoreUp(user.diem_thanh_vien)} điểm nữa bạn sẻ thăng hạng
-        </Text>
-        <Text style={[styles.txtfc, {fontSize: 12}]}>
-          Đổi quà không ảnh hưởng tới việc thăng hạng của bạn
-        </Text>
-        <Text style={[styles.txtfc, {fontSize: 12}]}>
-          Hãy dùng điểm để đổi ưu đãi nhé
-        </Text>
-        {/* </View> */}
-      </LinearGradient>
 
-      <View style={styles.card}>
-        <View style={styles.cardRow}>
-          <TouchableOpacity
-            onPress={() => navigation.navigate('WheelOfFortune')}
-            style={styles.cardExtention}>
-            <Icon
-              name="crown-outline"
-              style={[styles.icon, {color: 'orange', fontSize: 26}]}
-            />
-            <Text style={styles.txtExtention}>Vòng quay </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.cardExtention}
-            onPress={() => navigation.navigate('AllScore')}>
-            <Icon
-              name="gift-outline"
-              style={[styles.icon, {color: '#FF4500', fontSize: 26}]}
-            />
-            <Text style={styles.txtExtention}>Đổi điểm</Text>
+        <View style={[styles.txtfc2, {marginRight: 24}]}>
+          <Text style={styles.txtTitle}>Voucher của bạn</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('AllVoucher')}>
+            <Text style={styles.txtall}>Xem tất cả</Text>
           </TouchableOpacity>
         </View>
-        <View style={styles.cardRow}>
-          <TouchableOpacity
-            style={styles.cardExtention}
-            onPress={() => {
-              dispatch(getHistoryScoreFetch({id_user: user.id_user}));
-              navigation.navigate('ScoreHistory');
-            }}>
-            <Icon
-              name="file-search-outline"
-              style={[styles.icon, {color: '#FF8C00', fontSize: 26}]}
-            />
-            <Text style={styles.txtExtention}>Lịch sử điểm</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.cardExtention}>
-            <Icon
-              name="shield-account-outline"
-              style={[styles.icon, {color: '#1E90FF', fontSize: 26}]}
-            />
-            <Text style={styles.txtExtention}>Quyền Lợi của bạn</Text>
+
+        <FlatList
+          scrollEnabled={false}
+          style={styles.flatList}
+          data={[...allVoucher]?.slice(0, 4)}
+          renderItem={RenderItem}
+          keyExtractor={item => item._id}
+        />
+
+        <View style={[styles.txtfc2, {marginRight: 24}]}>
+          <Text style={styles.txtTitle}>Đổi Điểm</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('AllScore')}>
+            <Text style={styles.txtall}>Xem tất cả</Text>
           </TouchableOpacity>
         </View>
-      </View>
-
-      <View style={[styles.txtfc2, {marginRight: 24}]}>
-        <Text style={styles.txtTitle}>Voucher của bạn</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('AllVoucher')}>
-          <Text style={styles.txtall}>Xem tất cả</Text>
-        </TouchableOpacity>
-      </View>
-
-      <FlatList
-        scrollEnabled={false}
-        style={styles.flatList}
-        data={[...allVoucher]?.slice(0, 4)}
-        renderItem={RenderItem}
-        keyExtractor={item => item._id}
-      />
-
-      <View style={[styles.txtfc2, {marginRight: 24}]}>
-        <Text style={styles.txtTitle}>Đổi Điểm</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('AllScore')}>
-          <Text style={styles.txtall}>Xem tất cả</Text>
-        </TouchableOpacity>
-      </View>
-      <FlatList
-        scrollEnabled={false}
-        style={styles.flatList}
-        data={[...dataScore]?.slice(0, 4)}
-        renderItem={RenderItem2}
-        keyExtractor={item => item._id}
-      />
-    </ScrollView>
+        <FlatList
+          scrollEnabled={false}
+          style={styles.flatList}
+          data={[...dataScore]?.slice(0, 4)}
+          renderItem={RenderItem2}
+          keyExtractor={item => item._id}
+        />
+      </ScrollView>
+    )
   );
 };
 
