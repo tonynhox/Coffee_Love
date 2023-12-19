9a802a30665fc1c290a555af128cddfe515994ce
tonynhox <anhhuytran1470@gmail.com>
2023-12-19 11:40:42 +0700

    ok

--- src/components/others/cartPayment/ModalEditIn4.js
+++ src/components/others/cartPayment/ModalEditIn4.js
@@ -1,16 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Modal from 'react-native-modal';
 
-const ModalEditIn4 = ({ isVisible, setIsVisible, in4, setIn4 }) => {
-  const [data, setData] = useState({ ho_ten: '', so_dien_thoai: '' });
+const ModalEditIn4 = ({isVisible, setIsVisible, in4, setIn4}) => {
+  const [data, setData] = useState({ho_ten: '', so_dien_thoai: ''});
 
   const closeModal = () => {
     setIsVisible(false);
   };
 
   useEffect(() => {
-    setData((dt) => ({
+    setData(dt => ({
       ...dt,
       ho_ten: in4.ho_ten,
       so_dien_thoai: in4.so_dien_thoai,
@@ -26,29 +32,43 @@ const ModalEditIn4 = ({ isVisible, setIsVisible, in4, setIn4 }) => {
       swipeDirection="down"
       onSwipeComplete={closeModal}
       animationIn="slideInUp"
-      animationOut="slideOutDown"
-    >
+      animationOut="slideOutDown">
       <View style={styles.modalContent}>
         <Text style={styles.modalTitle}>Thay đổi tin cá nhân</Text>
         <View style={styles.inputContainer}>
-          <Text>Họ và tên</Text>
+          <Text
+            style={{
+              color: '#000',
+            }}>
+            Họ và tên
+          </Text>
           <TextInput
             style={styles.input}
             placeholder="Họ và tên"
             value={data.ho_ten}
-            onChangeText={(text) => setData({ ...data, ho_ten: text })}
+            onChangeText={text => setData({...data, ho_ten: text})}
           />
         </View>
         <View style={styles.inputContainer}>
-          <Text>Số điện thoại</Text>
+          <Text
+            style={{
+              color: '#000',
+            }}>
+            Số điện thoại
+          </Text>
           <TextInput
             style={styles.input}
             placeholder="Số điện thoại"
             value={data.so_dien_thoai}
-            onChangeText={(text) => setData({ ...data, so_dien_thoai: text })}
+            onChangeText={text => setData({...data, so_dien_thoai: text})}
           />
         </View>
-        <TouchableOpacity style={styles.saveButton} onPress={() => { setIn4(data); closeModal(); }}>
+        <TouchableOpacity
+          style={styles.saveButton}
+          onPress={() => {
+            setIn4(data);
+            closeModal();
+          }}>
           <Text style={styles.saveButtonText}>Lưu</Text>
         </TouchableOpacity>
       </View>
@@ -65,11 +85,15 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     padding: 20,
     borderRadius: 10,
+    transform: [{
+      scale:1.3
+    }],
   },
   modalTitle: {
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
+    color: '#000',
   },
   inputContainer: {
     marginBottom: 15,
